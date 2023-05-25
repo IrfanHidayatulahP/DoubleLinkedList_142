4d12cfc10991cff16a22c29f9f148ac6b3b008a9
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-25 15:55:33 +0700

    menambah kondisi current

--- Double Linked List/Double Linked List.cpp
+++ Double Linked List/Double Linked List.cpp
@@ -53,6 +53,15 @@ void DoubleLinkedList::addNode() {
 		START = newNode;														//step 6
 		return;
 	}
+
+	/*insert a Node Between Two Nodes in the List*/
+	Node* current = START;														//step 1.a
+	Node* previous = NULL;														//step 1.b
+	while (current->next != NULL && current->next->noMhs < nim)					//step 1.c
+	{
+		previous = current;														//step 1.d
+		current = current->next;												//step 1.e
+	}
 }
 
 
